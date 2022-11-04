979d91105bd798c9523f892b2b0eecbe24114f9b
tomthomas123 <tomthomas1248@gmail.com>
2022-11-04 09:11:57 +0530

    all error done

--- productapp.py
+++ productapp.py
@@ -15,5 +15,12 @@ data = requests.get("https://dummyjson.com/products").text
 
 data_info = json.loads(data)
 for i in data_info['products']:
+    price = str(i['price'])
+    discout = str(i['discountPercentage'])
+    rating = str(i['rating'])
+    stock = str(i['stock'])
+    sql = 'INSERT INTO `products`(`title`, `description`, `price`, `discount`, `rating`, `stock`, `brand`, `category`) VALUES ("'+i['title']+'","'+i['description']+'", "'+price+'","'+discout+'","'+rating+'","'+stock+'","'+i['brand']+'","'+i['category']+'")'
+    mycursor.execute(sql)
+    mydb.commit()
     print(str(i['title']))
 
